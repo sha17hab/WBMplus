a6c5872c7c2542db8c69a56348d74a22c44545b8
Balazs Fekete <>
2012-01-17 17:46:27 -0500

    Making virtual changes for testing git.

--- common.mk
+++ common.mk
@@ -3,7 +3,7 @@ ifndef GHAASDIR
 export GHAASDIR=/usr/local/share/ghaas
 endif
 
+export UNIXMAKE=make
 export UNIXCC=gcc
 export UNIXCCOPS=-g -Wall -pthread -fsigned-char -D_GNU_SOURCE
 export UNIXLIBS=-pthread -lm
-export UNIXMAKE=make
